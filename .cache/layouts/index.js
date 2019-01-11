7373f9e677728b6b8fec335978a2a3b191c37c63
lee <leeping88901024@gmail.com>
2019-01-11 11:24:22 +0800

    Initial commit

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "C:/Users/Lee/Desktop/blog2.0.0/contentful-gatsby-blog/src/layouts/index.js"
+  import data from "C:\\Users\\Lee\\Desktop\\blog2.0.0\\contentful-gatsby-blog\\.cache\\json\\layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
