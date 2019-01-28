327568680f6a5b07b96db631e3f6716ba0c4de5a
lee <leeping88901024@gmail.com>
2019-01-28 16:36:04 +0800

    complete navbar

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,15 +2,43 @@ import React from 'react'
 import Link from 'gatsby-link'
 import styles from './navigation.module.css'
 
-export default () => (
-  <header className={styles.navigation}>
-    <ul>
-      <li>
-        <Link to="/">主页</Link>
-      </li>
-      <li>
-        <Link to="/blog/">文章</Link>
-      </li>
-    </ul>
-  </header>
-)
+var prevScrollpos = window.pageYOffset
+
+export default class Navigation extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      curr: null
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+  
+  handleScroll = () => {
+    var currentScrollPos = window.scrollY;
+    this.setState({curr: currentScrollPos})
+    prevScrollpos = currentScrollPos;
+  }
+
+
+  render() {
+    return(
+      <header className={styles.navigation} style={this.state.curr <= prevScrollpos ? {top: '0'} : {top: '-40'} }>
+        <ul>
+          <li>
+            <Link to="/">主页</Link>
+          </li>
+          <li>
+            <Link to="/blog/">文章</Link>
+          </li>
+        </ul>
+      </header>
+    )
+  }
+}
